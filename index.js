71568fa9a38eb808c1460c1b939868d0ddede97c
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-28 20:40:22 +1000

    gendocs

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+/**
+  # urlparts
+
+  A simple url parser that uses the core node parser in a node environment,
+  and an `<a>` tag in a browserified environment for a nice lightweight
+  solution in a browser.
+
+  ## Example Usage
+
+  To be completed.
+
+**/
 module.exports = require('url').parse;
