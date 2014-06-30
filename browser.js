45330c1248ffb133cd004ea53cc3f0a6f4c2ef9b
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-30 16:30:19 +1000

    Implement querystring parsing

--- browser.js
+++ browser.js
@@ -1,14 +1,39 @@
 module.exports = (function(a) {
-  return function(url) {
+  var decode = decodeURIComponent;
+
+  function parseQueryString(input) {
+    var parts = input.split('&');
+    var output = {};
+
+    parts.forEach(function(pair) {
+      var nv = pair.split('=');
+      output[decode(nv[0])] = decode(nv[1] || '');
+    });
+
+    return output;
+  }
+
+  return function(url, qs) {
+    var output;
+
+    // inject the url into the tag
     a.href = url;
 
-    return {
+    // generate the output
+    output = {
       protocol: a.protocol,
       hostname: a.hostname,
       host: a.host,
-      path: a.pathname,
+      path: a.pathname + (a.search ? '?' + a.search : ''),
       pathname: a.pathname,
       port: a.port || '80'
     };
+
+    // add the query string if it should be parsed
+    if (qs) {
+      output.query = parseQueryString(a.search.slice(1));
+    }
+
+    return output;
   };
 })(document.createElement('a'));
